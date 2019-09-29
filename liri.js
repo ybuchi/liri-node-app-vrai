25e9783a53563ff82d77eb63245480bcbfe847f4
Yannik Buchi <yabuch_ch@hotmail.com>
2019-09-29 15:48:10 -0400

    Added functionality for the rest of the APIs. No functionality yet for the fs library. Need to display the bandsintown and OMDB info correctly

--- liri.js
+++ liri.js
@@ -1,6 +1,13 @@
 // Read and set any variables with the dotenv package
 require("dotenv").config();
 
+//Require the axios package
+var axios = require("axios");
+
+//Require the Bands In Town poackage
+var bandsintown = require("bandsintown")("codingbootcamp");
+
+
 // Import keys from keys.js and store them in a variable.
 var keys = require("./keys.js");
 
@@ -38,13 +45,44 @@ spotify
     console.log("-----Finding song information for: " + query + " -----");
     console.log("-------------------------------------------------------------");
 
-    console.log("Song: " + response.tracks.items[0].name);
-    console.log("Artist: " + response.tracks.items[0].artists[0].name);
-    console.log("Album: " + response.tracks.items[0].album.name);
+    for (i = 0; i < 10; i ++){
+        console.log("--------------------")
+        console.log("///Result " + (i+1));
+        console.log("--------------------")
+        console.log("Song: " + response.tracks.items[i].name);
+        console.log("Artist: " + response.tracks.items[i].artists[0].name);
+        console.log("Album: " + response.tracks.items[i].album.name);
+        console.log("Song Link: " + response.tracks.items[i].href);
+    }//End of For loop
   })
   .catch(function(err) {
     console.log(err);
-  });
+  });//End of Spotify API call
 
-};
+}else if(service === "movie-this"){
+    //include the OMDB API call here
+    axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+
+    console.log("Movie: " + response.data.Title);
+    console.log("Release Year: " + response.data.Released);
+    console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+    console.log("Country :" + response.data.Country);
+    console.log("Language :" + response.data.Language);
+    console.log("Movie plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+
+    // console.log("The movie's rating is: " + JSON.stringify(response.data, null, 2));
+  })//END of OMDB API call
+}else if(service === "concert-this"){
+    axios
+    .get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+
+        console.log(response);
+
+    })
+}
+
 
